c355632a0d277d0b89d203c6a65de56f5775e532
ctatineni <chandhut@gmail.com>
2019-07-23 12:32:06 -0500

    add 443 target group

--- modules/pks/lb.tf
+++ modules/pks/lb.tf
@@ -104,3 +104,21 @@ resource "aws_lb_target_group" "harbor_4443" {
   protocol = "TCP"
   vpc_id   = "${var.vpc_id}"
 }
+
+resource "aws_lb_listener" "harbor_443" {
+  load_balancer_arn = "${aws_lb.harbor.arn}"
+  port              = 443
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.harbor_443.arn}"
+  }
+}
+
+resource "aws_lb_target_group" "harbor_443" {
+  name     = "${var.env_name}-harbor-tg-443"
+  port     = 443
+  protocol = "TCP"
+  vpc_id   = "${var.vpc_id}"
+}
